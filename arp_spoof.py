fece8108fed251b4eef6c2ef3d660d211a224841
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-10 13:21:06 -0400

    Initial commit

---
+++ arp_spoof.py
@@ -0,0 +1,7 @@
+#!/usr/bin//env python
+
+import scapy.all as scapy
+
+#op=2 is a arp reqponse and not a request
+packet = scapy.ARP(op=2, pdst="10.0.2.4", hwdst="08:00:27:33:26:01", psrc="10.0.2.1")
+
