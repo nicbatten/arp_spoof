7b9b8a5c87cc474d48029a9b7af75251b7916ce7
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-10 13:23:41 -0400

    Printing a summary of the packet that was sent

--- arp_spoof.py
+++ arp_spoof.py
@@ -4,4 +4,5 @@ import scapy.all as scapy
 
 #op=2 is a arp reqponse and not a request
 packet = scapy.ARP(op=2, pdst="10.0.2.4", hwdst="08:00:27:33:26:01", psrc="10.0.2.1")
-
+print(packet.show())
+print(packet.summary())
